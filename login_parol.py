287ed5bf2f8582d04c955e113e17cfb855105ea7
MUkhammadali <mukhammadali0070@gmail.com>
2021-10-13 00:15:11 +0500

    databasega ulavoldik

---
+++ login_parol.py
@@ -0,0 +1,12 @@
+import mysql.connector
+import platform 
+import os
+import sys
+
+my_db = mysql.connector.connect(
+        host = 'localhost',
+        user = 'root',
+        password = 'qw0002004',
+        database = 'alii'
+        )
+
