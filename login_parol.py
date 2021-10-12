d4464d91d717aaf21c6103886b993eb00f883aa3
MUkhammadali <mukhammadali0070@gmail.com>
2021-10-13 00:22:02 +0500

    table ochvoldik

--- login_parol.py
+++ login_parol.py
@@ -1,12 +1,19 @@
 import mysql.connector
-import platform 
+import platform
 import os
 import sys
 
 my_db = mysql.connector.connect(
-        host = 'localhost',
-        user = 'root',
-        password = 'qw0002004',
-        database = 'alii'
-        )
+    host='localhost',
+    user='root',
+    password='qw0002004',
+    database='alii'
+)
 
+my_cursor = my_db.cursor()
+my_cursor.execute("CREATE TABLE IF NOT EXISTS login_parol(id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,"
+                  "first_name VARCHAR(30) NOT NULL,"
+                  "last_name VARCHAR(30) NOT NULL,"
+                  "age INT(3) NOT NULL,"
+                  "login VARCHAR(30) NOT NULL,"
+                  "password VARCHAR(30) NOT NULL)")
